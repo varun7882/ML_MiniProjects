a749bb2f77a472299ce8612d5b2e9f6373edaf56
Srivastava, Varun <Varun.Srivastava@harman.com>
2018-05-25 19:33:22 +0530

    accurate classification of wine

--- Wine Classification using Neural Networks/WineClassificationUsingNeuralNets.py
+++ Wine Classification using Neural Networks/WineClassificationUsingNeuralNets.py
@@ -28,106 +28,75 @@ X_test = sc.transform(X_test)
 m=len(X_train)
 # Now we define all our functions
 
-def softmax(z):
-    #Calculate exponent term first
-    exp_scores = np.exp(z)
-    return exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+def sigmoid(z):
+    #np.clip(z,-1,1)
+    return 1/(1+np.exp(-z))
 
-def softmax_loss(y,y_hat):
-    # Clipping value
-    minval = 0.000000000001
-    # Number of samples
-    m = y.shape[0]
-    # Loss formula, note that np.sum sums up the entire matrix and therefore does the job of two sums from the formula
-    loss = -1/m * np.sum(y * np.log(y_hat.clip(min=minval)))
-    return loss
-
-def loss_derivative(y,y_hat):
-    return (y-y_hat)
-
-def tanh_derivative(x):
-    return (1 - np.power(x, 2))
+def derivative_sigmoid(z):
+    #np.clip(z,-1,1)
+    return z*(1-z)
+    
+def initialize_parameters(nn_input_dim,nn_hdim,nn_output_dim):
+    print "initializaing parameters...."
+    # First layer weights
+    W1 = np.random.randn(nn_input_dim, nn_hdim) * 2.0/np.sqrt((nn_input_dim))
+    # First layer bias
+    b1 = np.random.randn(1, nn_hdim) * 2/np.sqrt((nn_input_dim))
+    
+    # Second layer weights
+    W2=np.random.randn(nn_hdim, nn_output_dim)* 2.0/np.sqrt((nn_hdim))
+    
+    b2 = np.random.randn(1,nn_output_dim) * 2.0/np.sqrt((nn_hdim))
+     
+    # Package and return model
+    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+    return model 
 
 # This is the forward propagation function
 def forward_prop(model,a0):
-    
-    #Start Forward Propagation
-    
-    # Load parameters from model
+    #print 'model is ',
+    #print model
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
-    
-    # Do the first Linear step 
-    # Z1 is the input layer x times the dot product of the weights + our bias b
     z1 = a0.dot(W1) + b1
-    
-    # Put it through the first activation function
-    a1 = np.tanh(z1)
-    
-    # Second linear step
+    a1 = sigmoid(z1)
     z2 = a1.dot(W2) + b2
-    
-    # Second activation function
-    a2 = softmax(z2)
-    
-    
+    a2 = sigmoid(z2)
     #Store all results in these values
     cache = {'a0':a0,'z1':z1,'a1':a1,'z2':z2,'a2':a2}
     return cache
 
 # This is the BACKWARD PROPAGATION function
 def backward_prop(model,cache,y):
-
-    # Load parameters from model
     W1, b1, W2, b2= model['W1'], model['b1'], model['W2'], model['b2']
-    
-    # Load forward propagation results
     a0,a1, a2 = cache['a0'],cache['a1'],cache['a2']
-    
-    
+    E2 = y-a2
     # Calculate loss derivative with respect to output
-    dz2 = y-a2
-
+    grad2=derivative_sigmoid(a2)
+    grad1=derivative_sigmoid(a1)
     # Calculate loss derivative with respect to second layer weights
-    dW2 = (a1.T).dot(dz2) #dW2 = 1/m*(a1.T).dot(dz2) 
-    
-    # Calculate loss derivative with respect to second layer bias
-    db2 = np.sum(dz2, axis=0,keepdims=True)
-    
-    dz1 = np.multiply(dz2.dot(W2.T),tanh_derivative(a1))
-    
-    dW1 = np.dot(a0.T,dz1)
-    
-    db1 = np.sum(dz1,axis=0,keepdims=True)
-    
+    d2=E2*grad2
+    dW2 = np.dot(a1.T,d2)
+    db2 = np.sum(d2, axis=0,keepdims=True)
+    E1=d2.dot(W2.T)
+    d1=E1*grad1
+    dW1 = np.dot(a0.T,d1)
+    db1 = np.sum(d1,axis=0,keepdims=True)
     # Store gradients
     grads = {'dW2':dW2,'db2':db2,'dW1':dW1,'db1':db1}
     return grads
 
 #TRAINING PHASE
-def initialize_parameters(nn_input_dim,nn_hdim,nn_output_dim):
-    # First layer weights
-    W1 = np.random.randn(nn_input_dim, nn_hdim)/np.sqrt(nn_input_dim)
-    
-    # First layer bias
-    b1 = np.zeros((1, nn_hdim))
-    
-    # Second layer weights
-    W2 = np.random.rand(nn_hdim, nn_output_dim)/np.sqrt(nn_hdim)
-    b2 = np.zeros((1,nn_output_dim))
-     
-    # Package and return model
-    model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
-    return model
+
 
 def update_parameters(model,grads,learning_rate):
     # Load parameters
     W1, b1, W2, b2= model['W1'], model['b1'], model['W2'], model['b2']
 
     # Update parameters
-    W1 -= learning_rate * grads['dW1']
-    b1 -= learning_rate * grads['db1']
-    W2 -= learning_rate * grads['dW2']
-    b2 -= learning_rate * grads['db2']
+    W1 += learning_rate * grads['dW1']
+    b1 += learning_rate * grads['db1']
+    W2 += learning_rate * grads['dW2']
+    b2 += learning_rate * grads['db2']
 
     
     # Store and return parameters
@@ -142,16 +111,19 @@ def predict(model, x):
     for i in ty:
         yy[k,i]=1
         k=k+1
-    return yy
+    return yy,ty
 def train(model,X_,y_,learning_rate, epochs=20000, print_loss=False):
     # Gradient descent. Loop over epochs
     for i in range(0, epochs):
-
+        #raw_input()
         # Forward propagation
         cache = forward_prop(model,X_)
+        #print 'cache is ',
+        #print cache
         #a1, probs = cache['a1'],cache['a2']
         # Backpropagation
-        
+        if i%100==0:
+            print "Loss: \n" + str(np.sqrt(np.sum(np.square(y_ - cache['a2']))))
         grads = backward_prop(model,cache,y_)
         # Gradient descent parameter update
         # Assign new parameters to the model
@@ -160,12 +132,18 @@ def train(model,X_,y_,learning_rate, epochs=20000, print_loss=False):
 np.random.seed(0)
 # This is what we return at the end
 model = initialize_parameters(nn_input_dim=13, nn_hdim= 6, nn_output_dim= 3)
-model = train(model,X_train,y_train,learning_rate=0.05,epochs=1000,print_loss=True)
-y_pred_val=predict(model,X_val)
-from sklearn.metrics import confusion_matrix
-#cm = confusion_matrix(y_val, y_pred_val)
-#print 'confusion matrix :'
-#print cm
+print 'Shapes of weights initaized W1',model['W1'].shape
+print 'Shapes of weights initaized b1',model['b1'].shape
+print 'Shapes of weights initaized W2',model['W2'].shape
+print 'Shapes of weights initaized b2',model['b2'].shape
+
+model = train(model,X_train,y_train,learning_rate=0.01,epochs=20000,print_loss=True)
+y_pred_val,ty=predict(model,X_val)
 from sklearn.metrics import f1_score
 print 'f-score(weighted) is : '
 print f1_score(y_val,y_pred_val,average='weighted')
+y_pred_test,ty=predict(model,X_test)
+from sklearn.metrics import f1_score
+print 'f-score(weighted) is : '
+print f1_score(y_test,y_pred_test,average='weighted')
+
